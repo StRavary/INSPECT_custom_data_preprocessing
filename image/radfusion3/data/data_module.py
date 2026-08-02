2ab8c6ea3e562b1c78557bee7091493fb6284633
Steven Ravary <54853545+StRavary@users.noreply.github.com>
2026-08-02 13:07:23 -0400

    refactor: centralize dataloader configuration to prevent HDF5 file-handle leaks and ensure fork-safety

--- image/radfusion3/data/data_module.py
+++ image/radfusion3/data/data_module.py
@@ -12,71 +12,67 @@ class DataModule(pl.LightningDataModule):
         self.dataset = builder.build_dataset(cfg)
         self.test_split = test_split
 
+    def _dataloader_kwargs(self):
+        nw = self.cfg.trainer.num_workers
+        return dict(
+            num_workers=nw,
+            persistent_workers=False,  # avoid HDF5 file-handle leaks across epochs
+            multiprocessing_context="spawn" if nw > 0 else None,  # h5py is not fork-safe
+            pin_memory=True,
+        )
+
     def train_dataloader(self):
         transform = builder.build_transformation(self.cfg, "train")
         dataset = self.dataset(self.cfg, split="train", transform=transform)
-        pw = self.cfg.trainer.num_workers > 0
+        kwargs = self._dataloader_kwargs()
         if getattr(self.cfg.dataset, "weighted_sample", False):
             sampler = dataset.get_sampler()
             return DataLoader(
                 dataset,
-                pin_memory=True,
                 drop_last=True,
                 shuffle=False,
                 sampler=sampler,
                 batch_size=self.cfg.dataset.batch_size,
-                num_workers=self.cfg.trainer.num_workers,
-                persistent_workers=pw,
+                **kwargs,
             )
         else:
             return DataLoader(
                 dataset,
-                pin_memory=True,
                 drop_last=True,
                 shuffle=True,
                 batch_size=self.cfg.dataset.batch_size,
-                num_workers=self.cfg.trainer.num_workers,
-                persistent_workers=pw,
+                **kwargs,
             )
 
     def val_dataloader(self):
         transform = builder.build_transformation(self.cfg, "val")
         dataset = self.dataset(self.cfg, split="valid", transform=transform)
-        pw = self.cfg.trainer.num_workers > 0
         return DataLoader(
             dataset,
-            pin_memory=True,
             drop_last=False,
             shuffle=False,
             batch_size=self.cfg.dataset.batch_size,
-            num_workers=self.cfg.trainer.num_workers,
-            persistent_workers=pw,
+            **self._dataloader_kwargs(),
         )
 
     def test_dataloader(self):
         transform = builder.build_transformation(self.cfg, self.test_split)
         dataset = self.dataset(self.cfg, split=self.test_split, transform=transform)
-        pw = self.cfg.trainer.num_workers > 0
         return DataLoader(
             dataset,
-            pin_memory=True,
             drop_last=False,
             shuffle=False,
             batch_size=self.cfg.dataset.batch_size,
-            num_workers=self.cfg.trainer.num_workers,
-            persistent_workers=pw,
+            **self._dataloader_kwargs(),
         )
 
     def all_dataloader(self):
         transform = builder.build_transformation(self.cfg, "all")
         dataset = self.dataset(self.cfg, split=self.cfg.test_split, transform=transform)
-        pw = self.cfg.trainer.num_workers > 0
         return DataLoader(
             dataset,
-            pin_memory=True,
             shuffle=False,
             drop_last=False,
             batch_size=self.cfg.dataset.batch_size,
-            num_workers=self.cfg.trainer.num_workers,
-            persistent_workers=pw,
+            **self._dataloader_kwargs(),
         )
